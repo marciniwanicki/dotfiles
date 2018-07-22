59921a99f33eabc0c0463c94525b88495b013c99
Marcin Iwanicki <marciniwanicki@192.168.1.108>
2018-07-22 17:41:58 +0100

    Complete installation script

---
+++ bin/clean.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+# Uninstall oh-my-zsh_history
+uninstall_oh_my_zsh 2> /dev/null
+
+# Remove all synbolic links
+for file in $HOME/.dotfiles/home/.*; do
+  rm -rf $HOME/$(basename $file) 2> /dev/null
+done;
+
+# Remove old .dotfiles
+rm -rf ~/.dotfiles
+rm -rf ~/.zshrc
---
+++ bin/install.sh
@@ -0,0 +1,112 @@
+#!/usr/bin/env bash
+
+function clean_up() {
+  ./clean.sh 2> /dev/null
+}
+
+function prepare_dir() {
+  mkdir ~/.dotfiles
+  cp -r ../home ~/.dotfiles/home
+}
+
+function install_homebrew() {
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+}
+
+function install_oh_my_zsh {
+  export ZSH="$HOME/.dotfiles/oh-my-zsh"
+
+  # Only enable exit-on-error after the non-critical colorization stuff,
+  # which may fail on systems lacking tput or terminfo
+  set -e
+
+  if ! command -v zsh >/dev/null 2>&1; then
+    printf "Zsh is not installed! Please install zsh first!\n"
+    exit
+  fi
+
+  if [ -d "$ZSH" ]; then
+    printf "You already have Oh My Zsh installed.\n"
+    printf "You'll need to remove $ZSH if you want to re-install.\n"
+    exit
+  fi
+
+  # Prevent the cloned repository from having insecure permissions. Failing to do
+  # so causes compinit() calls to fail with "command not found: compdef" errors
+  # for users with insecure umasks (e.g., "002", allowing group writability). Note
+  # that this will be ignored under Cygwin by default, as Windows ACLs take
+  # precedence over umasks except for filesystems mounted with option "noacl".
+  umask g-w,o-w
+
+  printf "Cloning Oh My Zsh...\n"
+  command -v git >/dev/null 2>&1 || {
+    echo "Error: git is not installed"
+    exit 1
+  }
+  # The Windows (MSYS) Git is not compatible with normal use on cygwin
+  if [ "$OSTYPE" = cygwin ]; then
+    if git --version | grep msysgit > /dev/null; then
+      echo "Error: Windows/MSYS Git is not supported on Cygwin"
+      echo "Error: Make sure the Cygwin git package is installed and is first on the path"
+      exit 1
+    fi
+  fi
+  env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git "$ZSH" || {
+    printf "Error: git clone of oh-my-zsh repo failed\n"
+    exit 1
+  }
+
+
+  printf "Looking for an existing zsh config...\n"
+  if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then
+    printf "Found ~/.zshrc.Backing up to ~/.zshrc.pre-oh-my-zsh\n";
+    mv ~/.zshrc ~/.zshrc.pre-oh-my-zsh;
+  fi
+
+  printf "Using the Oh My Zsh template file and adding it to ~/.zshrc\n"
+  cp "$ZSH"/templates/zshrc.zsh-template ~/.dotfiles/home/.zshrc
+  sed "/^export ZSH=/ c\\
+  export ZSH=\"$ZSH\"
+  " ~/.dotfiles/home/.zshrc > ~/.zshrc-omztemp
+  mv -f ~/.zshrc-omztemp ~/.dotfiles/home/.zshrc
+  ln -s ~/.dotfiles/home/.zshrc ~/.zshrc
+
+  for file in $HOME/.dotfiles/home/.{exports,aliases,commands,functions,history}; do
+    echo "source $file" >> ~/.dotfiles/home/.zshrc
+  done;
+
+  # If this user's login shell is not already "zsh", attempt to switch.
+  TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
+  if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
+    # If this platform provides a "chsh" command (not Cygwin), do it, man!
+    if hash chsh >/dev/null 2>&1; then
+      printf "Time to change your default shell to zsh!\n"
+      chsh -s $(grep /zsh$ /etc/shells | tail -1)
+    # Else, suggest the user do so manually.
+    else
+      printf "I can't change your shell automatically because this system does not have chsh.\n"
+      printf "Please manually change your default shell to zsh!\n"
+    fi
+  fi
+
+  for file in $HOME/.dotfiles/home/.*; do
+    filename=$(basename $file)
+    if [ $filename = ".zshrc" ] || [ $filename = "." ] || [ $filename = ".." ]; then
+      continue
+    fi
+    ln -s $file $HOME/$(basename $file)
+  done;
+
+  echo 'Oh My Zsh is now installed'
+
+  env zsh -l
+}
+
+function main() {
+  clean_up
+  prepare_dir
+  install_homebrew
+  install_oh_my_zsh
+}
+
+main
