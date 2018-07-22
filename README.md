f1b9a3fee477e8ec1dbc2f5d4bb16852bb9fc176
Marcin Iwanicki <marcin@iwanicki.me>
2018-07-22 12:04:53 +0100

    Some progress

---
+++ README.md
@@ -0,0 +1,17 @@
+# .dotfiles
+
+The repository contains a mix of popular open sourced dotfiles shared
+on github as well as my own random setup.
+**To be clear, I do not recommend you to fork that project :-)** First, there are much
+better and well maintained dotfiles out there, second, you might end up
+installing lots of stuff on you machine that will never be used. It's probably
+better if you spend some time reading the files and crafting new ones
+fitting best your own needs. I bet you will do it well and even learn some
+new shell tricks.
+
+## The real .files ninjas, Huuuge thanks to all of them
+- Mathias Bynens - https://github.com/mathiasbynens/dotfiles (most of .aliases
+  and brew.sh lines are coming from his repository)
+
+## License
+All files in this repository are released under the MIT license. [See LICENSE](https://github.com/marciniwanicki/dotfiles/blob/develop/LICENSE) for details.
---
+++ bin/brew.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Install cask
+brew tap caskroom/cask
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+# https://www.gnu.org/software/coreutils/coreutils.html
+brew install coreutils
+
+# Install some other useful utilities like `sponge`.
+# sponge reads standard input and writes it out to the specified file.
+# Unlike a shell redirect, sponge soaks up all its input before opening
+# the output file. This allows constricting pipelines that read from and
+# write to the same file.
+brew install moreutils
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+
+# Install `wget`
+brew install wget
+
+# Install Java
+brew cask install java
+
+# Install font tools.
+brew tap bramstein/webfonttools
+brew install sfnt2woff
+brew install sfnt2woff-zopfli
+brew install woff2
+
+# Install some CTF tools; see https://github.com/ctfs/write-ups.
+brew install aircrack-ng
+brew install bfg
+brew install binutils
+brew install binwalk
+brew install cifer
+brew install dex2jar
+brew install dns2tcp
+brew install fcrackzip
+brew install foremost
+brew install hashpump
+brew install hydra
+brew install john
+brew install knock
+brew install netpbm
+brew install nmap
+brew install pngcheck
+brew install socat
+brew install sqlmap
+brew install tcpflow
+brew install tcpreplay
+brew install tcptrace
+brew install ucspi-tcp # `tcpserver` etc.
+brew install xpdf
+brew install xz
+
+# Install other useful binaries.
+brew install ack
+brew install git
+brew install git-lfs
+brew install imagemagick --with-webp
+brew install lua
+brew install lynx
+brew install p7zip
+brew install pigz
+brew install pv
+brew install rename
+brew install rlwrap
+brew install ssh-copy-id
+brew install tree
+brew install vbindiff
+brew install zopfli
+
+# Ruby
+gem install bundler
+
+# Remove outdated versions from the cellar.
+brew cleanup
