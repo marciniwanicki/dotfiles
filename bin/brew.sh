60b0f3579c3a6624ad00306684a9acea0e5baac3
Marcin Iwanicki <iwan@icode.pl>
2025-09-23 01:29:01 +0100

    Move brew packages to separtae file

--- bin/brew.sh
+++ bin/brew.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env zsh -e
 
+# Load brew configurations
+source $HOME/.dotfiles/.brew
+source $HOME/.dotfiles/.brew.local
+
 # Make sure we’re using the latest Homebrew
 brew update
 
@@ -10,50 +14,18 @@ brew upgrade
 brew upgrade --cask
 
 # Casks (GUI apps) and fonts
-casks=(
-  iterm2                     # Install iTerm2 for a better terminal experience
-  font-meslo-lg-nerd-font    # Install Meslo LG Nerd Font for terminal and editor
-  font-jetbrains-mono        # Install JetBrains Mono font for coding
-  visual-studio-code         # Install VSCode for a powerful code editor
-)
+all_casks=("${casks[@]}" "${local_casks[@]}")
 
 # Formulae (CLI tools)
-formulae=(
-  python3                  # Install Python for scripting and development
-  go                       # Install Go for programming
-  starship                 # Install starship for a fast shell prompt
-  zsh-syntax-highlighting  # Install must-have Zsh extensions
-  stow                     # Install stow for .dotfiles linking
-  zoxide                   # Install zoxide for easier navigation
-  fzf                      # Install fzf for better fuzzy finding
-  wget                     # Install wget for downloading files from the web
-  dpkg                     # Install dpkg for managing Debian packages
-  npm                      # Install npm for managing Node.js packages
-  cloc                     # Install cloc for counting lines of code
-  scc                      # Install scc for counting lines of code (faster alternative to cloc)
-  jq                       # Install jq for processing JSON
-  gh                       # Install gh for GitHub CLI
-  ripgrep                  # Install ripgrep for searching files
-  fd                       # Install fd for a simple, fast and user-friendly alternative to 'find'
-  mc                       # Install mc for a modern replacement for 'ls'
-  tree                     # Install tree for displaying directory structures
-  tre                      # Install tre for displaying directory structures
-  tmux                     # Install tmux for terminal multiplexing
-  neovim                   # Install neovim for a modern text editor
-  bat                      # Install bat for a cat clone with syntax highlighting and Git integration
-  tldr                     # Install tldr for simplified and community-driven man pages
-  speedtest-cli            # Install speedtest-cli for testing internet bandwidth
-  duf                      # Install duf for a better 'df' alternative
-  dua-cli                  # Install dua-cli for disk usage analysis (@piotrwach is a contributor)
-)
+all_formulae=("${formulae[@]}" "${local_formulae[@]}")
 
 # Install missing casks only
-for pkg in "${casks[@]}"; do
+for pkg in "${all_casks[@]}"; do
     brew list --cask "$pkg" >/dev/null 2>&1 || brew install --cask "$pkg"
 done
 
 # Install missing formulae only
-for pkg in "${formulae[@]}"; do
+for pkg in "${all_formulae[@]}"; do
     brew list "$pkg" >/dev/null 2>&1 || brew install "$pkg"
 done
 
